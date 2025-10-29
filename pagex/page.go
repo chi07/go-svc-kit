4590ea807d1b6a1cd8d691dfc9a7cd262b8211b1
chi07 <chipv.bka@gmail.com>
2025-10-29 16:57:52 +0700

    update paging

--- pagex/page.go
+++ pagex/page.go
@@ -10,27 +10,45 @@ type PageInfo struct {
 	HasPrevious bool  `json:"hasPrevious"`
 }
 
+func clampNonNeg(x int64) int64 {
+	if x < 0 {
+		return 0
+	}
+	return x
+}
+
+func ceilDiv(a, b int64) int64 {
+	if b <= 0 {
+		return 0
+	}
+	return (a + b - 1) / b
+}
+
 func (p *PageInfo) Compute() {
 	if p.Limit <= 0 {
 		p.Limit = 10
 	}
+	p.Offset = clampNonNeg(p.Offset)
+
 	if p.Total <= 0 {
 		p.TotalPages = 1
 		p.CurrentPage = 1
-		p.HasNext = false
 		p.HasPrevious = p.Offset > 0
+		p.HasNext = false
 		return
 	}
-	p.TotalPages = (p.Total + p.Limit - 1) / p.Limit
+
+	p.TotalPages = ceilDiv(p.Total, p.Limit)
+
 	p.CurrentPage = (p.Offset / p.Limit) + 1
-	if p.CurrentPage > p.TotalPages {
-		p.CurrentPage = p.TotalPages
-	}
 	if p.CurrentPage < 1 {
 		p.CurrentPage = 1
+	} else if p.CurrentPage > p.TotalPages {
+		p.CurrentPage = p.TotalPages
 	}
-	p.HasPrevious = p.CurrentPage > 1
-	p.HasNext = p.CurrentPage < p.TotalPages
+
+	p.HasPrevious = p.Offset > 0
+	p.HasNext = (p.Offset + p.Limit) < p.Total
 }
 
 func PageToOffset(page, limit int64) int64 {
@@ -38,4 +56,62 @@ func PageToOffset(page, limit int64) int64 {
 		return 0
 	}
 	return (page - 1) * limit
+}
+
+func Normalize(limit, offset int64) (int64, int64) {
+	return clampNonNeg(limit), clampNonNeg(offset)
+}
+
+func FromTotal(limit, offset, total int64) PageInfo {
+	limit, offset = Normalize(limit, offset)
+	pi := PageInfo{
+		Limit:  limit,
+		Offset: offset,
+		Total:  total,
+	}
+
+	if total <= 0 {
+		pi.TotalPages = 1
+		pi.CurrentPage = 1
+		pi.HasPrevious = offset > 0
+		pi.HasNext = false
+		return pi
+	}
+
+	if limit <= 0 {
+		pi.TotalPages = 1
+		pi.CurrentPage = 1
+		return pi
+	}
+
+	pi.TotalPages = ceilDiv(total, limit)
+	pi.CurrentPage = (offset / limit) + 1
+	if pi.CurrentPage < 1 {
+		pi.CurrentPage = 1
+	} else if pi.CurrentPage > pi.TotalPages {
+		pi.CurrentPage = pi.TotalPages
+	}
+
+	pi.HasPrevious = offset > 0
+	pi.HasNext = (offset + limit) < total
+	return pi
+}
+
+func FromLookahead(limit, offset, rowsLen int64) PageInfo {
+	limit, offset = Normalize(limit, offset)
+	pi := PageInfo{
+		Limit:       limit,
+		Offset:      offset,
+		HasPrevious: offset > 0,
+	}
+
+	if limit > 0 {
+		pi.CurrentPage = (offset / limit) + 1
+		pi.HasNext = rowsLen > limit
+	} else {
+		pi.CurrentPage = 1
+		pi.HasNext = false
+	}
+
+	return pi
 }
